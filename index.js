cc22b147bdf6f56fbcac4b5cd05f0f5b66261d93
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2020-11-06 09:45:59 -0800

    finished task 5 in index.js

--- index.js
+++ index.js
@@ -270,8 +270,11 @@ Create a function called `removeArtist` that takes two arguments:
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset and log the number 19.  
 */
-function removeArtist(/*Your Code Here*/) {
+function removeArtist(artistsArray,index) {
    /*Your Code Here*/
+   artistsArray.splice(index,1);
+   console.log(artistsArray.length);
+   return artistsArray.length;
 }
    
 
